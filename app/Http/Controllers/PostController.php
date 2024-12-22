9c608d78c37545907aaba2ea2d64a9678eead8d4
Vignesh Ravichandran <114295235+vickyvk326@users.noreply.github.com>
2024-12-22 15:06:19 +0530

    fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,7 +29,6 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        sleep(2);
         $fields = $request->validate([
             'body' => ['required'],
         ]);
